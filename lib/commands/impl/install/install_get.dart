4cef77c5e9f24c7db983841d8929b0288e5af3dc
bigbott <u.novicow@gmail.com>
2025-10-14 18:52:39 +0300

    changed to GetX 5 last version

--- lib/commands/impl/install/install_get.dart
+++ lib/commands/impl/install/install_get.dart
@@ -1,6 +1,18 @@
 import '../../../common/utils/pubspec/pubspec_utils.dart';
 
+// Future<void> installGet([bool runPubGet = false]) async {
+//   PubspecUtils.removeDependencies('get', logger: false);
+//   await PubspecUtils.addDependencies('get', version: getVersion, runPubGet: runPubGet);
+// }
+
+///Yuriy: changed to GetX 5 last version
+String getVersion = '^5.0.0-release-candidate-9.3.2';
+//String getVersion = '>=5.0.0-release-candidate-9.3.2<6.0.0';
 Future<void> installGet([bool runPubGet = false]) async {
   PubspecUtils.removeDependencies('get', logger: false);
-  await PubspecUtils.addDependencies('get', runPubGet: runPubGet);
+  await PubspecUtils.addDependencies(
+    'get',
+    version: getVersion,
+    runPubGet: runPubGet,
+  );
 }
